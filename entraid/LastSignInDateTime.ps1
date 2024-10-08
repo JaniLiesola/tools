5dac731efddcf0ba0aee9123f4efeee3da37fe85
Jani Liesola <jani.liesola@elisa.fi>
2024-10-08 09:03:30 +0000

    feat: add companyNane and accountEnabled to LastSignInDateTime.ps1

--- entraid/LastSignInDateTime.ps1
+++ entraid/LastSignInDateTime.ps1
@@ -6,7 +6,7 @@ Retrieves and exports a list of users who have not signed in within the last 90 
 This script connects to Microsoft Graph with the required scopes, retrieves users who have not signed in within the last 90 days, and exports the selected user information to a CSV file.
 
 .EXAMPLE
-.\Get-LastSignInDateTime.ps1
+.\LastSignInDateTime.ps1
 This example runs the script and exports the list of inactive users to "inactive_users.csv".
 
 .NOTES
@@ -15,6 +15,6 @@ Ensure you have the necessary permissions to access Microsoft Graph API and the 
 
 Connect-MgGraph -Scopes "User.Read.All", "AuditLog.Read.All"
 $date = [DateTime]::Today.AddDays(-90)
-$users = Get-MGUser  -Filter "signInActivity/lastSignInDateTime le $($date.ToUniversalTime().ToString("yyyy-MM-ddThh:mm:ssZ"))" -Select Id, userPrincipalName, mail, signInActivity
-$selectedUsers = $users | SELECT ID, mail, userprincipalname, @{N="lastSignInDateTime"; e={$_.signInActivity.lastSignInDateTime}}
+$users = Get-MGUser  -Filter "signInActivity/lastSignInDateTime le $($date.ToUniversalTime().ToString("yyyy-MM-ddThh:mm:ssZ"))" -Select Id, userPrincipalName, mail, signInActivity, companyName, accountEnabled
+$selectedUsers = $users | SELECT ID, mail, userprincipalname, companyName, accountEnabled, @{N="lastSignInDateTime"; e={$_.signInActivity.lastSignInDateTime}}
 $selectedUsers | Export-Csv -Path "inactive_users.csv" -NoTypeInformation
